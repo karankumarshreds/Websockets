8f6432887f54c55cc001f439a9e30d2427bfd268
karankumarshreds <naileditnerd@gmail.com>
2022-05-07 17:14:05 +0530

    refactor code for redis interaction

--- server/services/redis.go
+++ server/services/redis.go
@@ -34,42 +34,30 @@ type UserNode struct {
 		Socket *websocket.Conn
 }
 
+type UserData map[string]UserNode
+
 func NewRedisService(rdb *redis.Client) *RedisService {
-	type UserNode struct {
-		Username string
-		Socket *websocket.Conn
-	}
-	d := make(map[string]UserNode)
-	if result, getErr := rdb.Get(USERS_DATA).Result(); getErr != nil {
-		log.Println("Unable to check initial user data in redis")
-		return nil
-	} else if result == "{}" {
-		log.Println("The map already exists as with the default value of {}")
-	} else {
-		// set default values as NONE  
-		if usersDataDefault, marshalErr := json.Marshal(d); marshalErr != nil {
-			log.Fatal("Unable to marshal initial user data to save in redis", marshalErr)
-			return nil 
-		} else {
-				if err := rdb.Set(USERS_DATA, usersDataDefault, 0).Err(); err != nil {
-				log.Fatal("Unable to create an intial user data in redis", err)
-				return nil 
-			} else {
-				result, _ := rdb.Get(USERS_DATA).Result()
-				log.Println("Successfully created initial user data in redis", result)
-			}
-		}
-	}
-	// TODO create an empty map for the chat data
 	return &RedisService{rdb}
 }
 
-func (r *RedisService) SetUserRedis(username string) {
-	log.Printf("The user with the username %v will be set in redis!", username)
-	// get the entire map from redis 
-	// create a copy of the entire map 
-	// add a key value for the new user 
-	// set the updated map in redis 
+func (r *RedisService) SetUserRedis(userid string, username string, socket *websocket.Conn) {
+	log.Printf("Setting user with the username %v in redis!", username)
+	userNode := UserNode{
+		Username: username,
+		Socket: socket,
+	}
+
+	// create a map in redis with the name of the userid
+	if jsonData, marshalErr  := json.Marshal(userNode); marshalErr != nil {
+		log.Println("ERR: Unable to marshal user data to set a map in redis", marshalErr)
+		return 
+	} else {
+		if err := r.rdb.Set(userid, jsonData, 0).Err(); err != nil {
+			log.Println("ERR: Unable to set the data in the redis for new user", err)
+			return 
+		}
+	}
+	log.Println("Successfully set the data for the user in redis map!")
 }
 
 func (r *RedisService) RemoveUserRedis() {
